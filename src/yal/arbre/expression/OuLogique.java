3a10088d3a729e0d31d54e60ed447805d32f32b9
schmit237u <jonathan.schmitt9@etu.univ-lorraine.fr>
2018-01-24 15:55:11 +0100

    patched 'or' eval

--- src/yal/arbre/expression/OuLogique.java
+++ src/yal/arbre/expression/OuLogique.java
@@ -25,8 +25,19 @@ public class OuLogique extends BinaireLogique {
 
 	@Override
 	public String toMIPS() {
-		// TODO Auto-generated method stub
-		return null;
+		return "# Evaluation de l'opérande gauche \n" +
+				gauche.toMIPS() + "\n" + 
+				"# Stocke la valeur de v0 dans la mémoire \n" +
+				"sw $v0, ($sp) \n" +
+				"# Dépilement dans $sp \n" +
+				"addi $sp, $sp -4 \n" +
+				"# Evaluation de l'opérande droite \n" +
+				droite.toMIPS() + "\n" +
+				"# Déplacement de sp \n" +
+				"addi $sp, $sp, +4 \n" +
+				"lw $t8, ($sp) \n" +
+				"# comparaison de t8 et v0 (ou) \n" +
+				"or $v0, $t8, $v0\n" ;
 	}
 
 }
