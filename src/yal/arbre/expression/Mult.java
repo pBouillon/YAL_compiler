40c9f7670e3a460222eb8066507704d59664c3b3
schmit237u <jonathan.schmitt9@etu.univ-lorraine.fr>
2018-01-24 14:29:15 +0100

    patched multiplication

--- src/yal/arbre/expression/Mult.java
+++ src/yal/arbre/expression/Mult.java
@@ -26,22 +26,26 @@ public class Mult extends BinaireArithmetique {
 	@Override
 	public String toMIPS() {
 		StringBuilder s = new StringBuilder();
-		s.append("#Evaluation de l'opérande gauche \n");
-		s.append(gauche.toMIPS());
-		s.append("\n");
-		s.append("#Stocke la valeur de v0 dans la mémoire \n");
-		s.append("sw $v0, ($sp)");
-		s.append("\n");
-		s.append("#Evaluation de l'opérande droite \n");
-		s.append( droite.toMIPS() );
-		s.append("\n");
-		s.append("#Recuperation de sp dans t8 \n");
-		s.append("lw $t8, ($sp) ");
-		s.append("\n");
-		s.append("#Multiplication de t8 par v0 \n");
-		s.append("mult $v0, $t8");
-		s.append("\n");
-		s.append("mflo $v0");
+		s.append("#Evaluation de l'opérande gauche \n")
+		 .append(gauche.toMIPS())
+		 .append("\n")
+		 .append("#Stocke la valeur de v0 dans la mémoire \n")
+		 .append("sw $v0, ($sp)")
+		 .append("\n")
+		 .append("addi $sp, $sp, -4")
+		 .append("\n")
+		 .append("#Evaluation de l'opérande droite \n")
+		 .append( droite.toMIPS() )
+		 .append("\n")
+		 .append("#Recuperation de sp dans t8 \n")
+		 .append("addi $sp, $sp, 4")
+		 .append("\n")
+		 .append("lw $t8, ($sp) ")
+		 .append("\n")
+		 .append("#Multiplication de t8 par v0 \n")
+		 .append("mult $v0, $t8")
+		 .append("\n")
+		 .append("mflo $v0");
 		return s.toString();
 	}
 
