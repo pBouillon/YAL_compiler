77a15e35badd3eed63c5f558bdc5e111d349755a
jonathan <jonathan.schmitt9@etu.univ-lorraine.fr>
2018-01-30 16:57:39 +0100

    patched !=

--- src/yal/arbre/expression/Different.java
+++ src/yal/arbre/expression/Different.java
@@ -35,13 +35,13 @@ public class Different extends Comparaison {
 				"\n" +
 				"sub $v0, $t8, $v0\n" +
 				"# verifie si v0 != t8\n" +
-				"bgtz $v0,"+ EtiquetteFactory.getInstance().getNextSiNeg()+"\n" +
+				"beqz $v0,"+ EtiquetteFactory.getInstance().getNextSiNeg()+"\n" +
 				"j "+EtiquetteFactory.getInstance().getNextSiNonNeg()+"\n" +
 				EtiquetteFactory.getInstance().getSiNeg()+":\n" +
-				"li $v0, 1\n" +
+				"li $v0, 0\n" +
 				"j "+EtiquetteFactory.getInstance().getNextFinSiNeg()+"\n" +
 				EtiquetteFactory.getInstance().getSiNonNeg()+":\n" +
-				"li $v0, 0\n" +
+				"li $v0, 1\n" +
 				EtiquetteFactory.getInstance().getFinSiNeg()+":\n";
 	}
 
