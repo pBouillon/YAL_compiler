d9362aa43bf4308b256e7605b5f281013a8ffe5f
pBouillon <pierre.bouillon@openmailbox.org>
2018-02-08 13:17:09 +0100

    Cleaned main

--- src/yal/Yal.java
+++ src/yal/Yal.java
@@ -20,52 +20,72 @@ import yal.exceptions.SemantiqueException;
 
 public class Yal {
 
-	public Yal(String fichier) {
-		try {
-			AnalyseurSyntaxique analyseur = new AnalyseurSyntaxique(new AnalyseurLexical(new FileReader(fichier)));
-			ArbreAbstrait arbre = (ArbreAbstrait) analyseur.parse().value;
-			System.err.println("expression stockée dans l'yal.arbre : " + arbre);
+	private static final String COMP_OK = "COMPILATION_OK" ;
 
-			// à écrire pour yal0
+	private static final int ARG_REQUIRED = 1 ;
+	private static final int EXIT_FAILURE = 1 ;
+
+
+    /**
+     * References Yal
+     * @param fichier .yal to compile
+     */
+	private Yal (String fichier) {
+		try {
+			AnalyseurSyntaxique analyseur = new AnalyseurSyntaxique(
+					new AnalyseurLexical (
+							new FileReader(fichier)
+					)
+			) ;
+			ArbreAbstrait arbre = (ArbreAbstrait) analyseur.parse().value ;
+			System.err.println ("expression stockée dans l'yal.arbre : " + arbre) ;
+
 			arbre.verifier() ;
 
-			ListeSemantiqueException liste = ListeSemantiqueException.getInstance();
-			if(liste.isError()) {
+			ListeSemantiqueException liste = ListeSemantiqueException.getInstance() ;
+			if (liste.isError()) {
 				for(SemantiqueException s : liste.getListeException()) {
-					System.err.println(s.getMessage());
+					System.err.println (s.getMessage()) ;
 				}
-			}else {
-				String fileName = fichier.substring(0,fichier.length()-4);
-				PrintWriter out = new PrintWriter(fileName + ".mips");
-				out.println(
+			} else {
+				String fileName = fichier.substring (0, fichier.length() - 4) ;
+				PrintWriter out = new PrintWriter (fileName + ".mips") ;
+				out.println (
 						header() +
 						arbre.toMIPS() +
 						footer()
-						);
-				out.close();
-				System.out.println("COMPILATION OK");
+					) ;
+				out.close() ;
+				System.out.println (COMP_OK) ;
 			}
 		} 
 
 		catch (FileNotFoundException ex) {
-			System.err.println("Fichier " + fichier + " inexistant") ;
+			System.err.println ("Fichier " + fichier + " inexistant") ;
 		}
 		catch (AnalyseException ex) {
 			System.err.println(ex.getMessage());
 		}
 		catch (Exception ex) {
-			Logger.getLogger(Yal.class.getName()).log(Level.SEVERE, null, ex);
+			Logger.getLogger (Yal.class.getName())
+					.log(Level.SEVERE, null, ex) ;
 		}
 	}
 
+    /**
+     * @return mips prog begin and data declarations
+     */
 	private String header() {
 		return  ".data : \n"+
-				"errdiv: 	.asciiz \"Division par zero impossible \" \n"+
+				"errdiv: 	.asciiz \"Division par zero impossible\" \n"+
 				".text\n" +
 				"main:\n" +
 				"move $s7, $sp\n" ;
 	}
 
+    /**
+     * @return mips clean ending and storage in $v1
+     */
 	private String footer() {
 		return  "\n\nend:\n" +
 				"move $v1, $v0\n" +
@@ -74,10 +94,10 @@ public class Yal {
 	}
 
 	public static void main(String[] args) {
-		if (args.length != 1) {
-			System.err.println("Nombre incorrect d'arguments") ;
-			System.err.println("\tjava -jar yal.jar <fichierSource.yal>") ;
-			System.exit(1) ;
+		if (args.length != ARG_REQUIRED) {
+			System.err.println ("Nombre incorrect d'arguments") ;
+			System.err.println ("\tjava -jar yal.jar <fichierSource.yal>") ;
+			System.exit(EXIT_FAILURE) ;
 		}
 		new Yal(args[0]) ;
 	}
