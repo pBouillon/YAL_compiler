730fa6ade4efe62404f014bf7dbf54b68f1755c8
kaysou <schmittjonathan@hotmail.fr>
2018-02-13 16:48:47 +0100

    modified affectation

--- src/yal/arbre/instruction/Affectation.java
+++ src/yal/arbre/instruction/Affectation.java
@@ -3,8 +3,14 @@ package yal.arbre.instruction;
 import yal.arbre.expression.Expression;
 
 public class Affectation extends Instruction {
+	
+	private Expression exp;
+	private String idf;
+	
     public Affectation(String idf, Expression v, int no) {
         super(no);
+        this.idf = idf;
+        this.exp = v;
     }
 
     @Override
@@ -14,6 +20,10 @@ public class Affectation extends Instruction {
 
     @Override
     public String toMIPS() {
-        return null;
+        return  +
+        		"# Affectation de " +
+        		" "+
+        		" "+
+        		" ";
     }
 }
