c832af88d8b3385b2737b1f337c298e378d4f3f7
pBouillon <pierre.bouillon@openmailbox.org>
2018-02-27 19:15:23 +0100

    WiP patched div

--- src/yal/arbre/expression/Div.java
+++ src/yal/arbre/expression/Div.java
@@ -24,15 +24,36 @@ public class Div extends BinaireArithmetique {
 
 	@Override
 	public String toMIPS() {
+		EtiquetteFactory et = EtiquetteFactory.getInstance() ;
+
+		return String.join(
+			"\n",
+				"\t# Division",
+				gauche.toMIPS(),
+				"\tsw $v0, ($sp)",
+				"\taddi $sp, $sp, -4",
+				droite.toMIPS(),
+				"\tbnez $v0, " + et.getNextSinon(),
+				et.getSi() + ":",
+				"\tli $v0, 4",
+				"la $a0, "+ DataFactory.getInstance().getErrDiv(),
+				"j end",
+				"syscall",
+				et.getSinon() + ":",
+				"\taddi $sp, $sp, 4",
+				"\tlw $t8, ($sp)",
+				"\tdiv $v0, $t8, $v0",
+				"\tsw $v0, ($sp)"
+		) ;
+/*
 		StringBuilder s = new StringBuilder();
 			s.append(gauche.toMIPS())
 			.append("\n")
-			.append("sw $v0, ($sp)")
-			.append("\n")
+			.append("sw $v0, ($sp)\n")
+			.append("addi $sp, $sp, -4\n")
 			.append( droite.toMIPS() )
 			.append("\n")
-			.append("lw $t8, ($sp) ")
-			.append("\n")
+			.append("lw $t8, ($sp)\n")
 			.append("beqz $v0,"+ EtiquetteFactory.getInstance().getNextSiNeg()+"\n")
 			.append("j " + EtiquetteFactory.getInstance().getNextSiNonNeg() +"\n")
 			.append(EtiquetteFactory.getInstance().getSiNeg()+ ":\n")
@@ -45,7 +66,8 @@ public class Div extends BinaireArithmetique {
 			.append("j " + EtiquetteFactory.getInstance().getNextFinSiNeg()+"\n")
 			.append(EtiquetteFactory.getInstance().getFinSiNeg()+":\n");
 			return s.toString();
-	}    
+*/
+	}
 	
 	@Override
 	public String getType() {
