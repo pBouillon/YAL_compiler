f5af557471bb7a3f7a6b4d3d25a0fe00b57b4037
yoanf <yoanfath@laposte.net>
2018-03-20 11:01:00 +0100

    WIP parent TDS for local var

--- src/yal/tabledessymboles/TDS.java
+++ src/yal/tabledessymboles/TDS.java
@@ -1,5 +1,6 @@
 package yal.tabledessymboles;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -11,11 +12,13 @@ import yal.exceptions.VariableUndefinedException;
 public class TDS {
 	private HashMap<Entree,Symbole> map;
 	private int compteur;
-
+	private ArrayList<TDS> tds;
+	
 	private TDS()
 	{
 		this.map = new HashMap<>();
 		compteur = 0 ;
+		this.tds = new ArrayList<>();
 	}
 
 	private static TDS INSTANCE = new TDS();
@@ -33,6 +36,7 @@ public class TDS {
 	}
 
 	public Symbole identifier(String e, int ligne){ 
+		// partir de la TDS de la fonction puis appeler la mere
 		boolean trouve = false;
 		Symbole res = null;
 
@@ -59,5 +63,14 @@ public class TDS {
 		this.compteur = compteur;
 	}
 
+	public ArrayList<TDS> getTds() {
+		return tds;
+	}
+
+	public void setTds(ArrayList<TDS> tds) {
+		this.tds = tds;
+	}
+
+	
 
 }
