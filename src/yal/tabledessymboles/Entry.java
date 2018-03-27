14116313c1e23517d7500bdc92c58b3f8b5a5983
pBouillon <pierre.bouillon@openmailbox.org>
2018-03-27 16:24:28 +0200

    Patched cup

--- src/yal/tabledessymboles/Entry.java
+++ src/yal/tabledessymboles/Entry.java
@@ -32,6 +32,8 @@ public abstract class Entry {
     public boolean equals(Object obj) {
         if (this == obj) return true ;
 
+        if (obj == null) return false ;
+
         if (getClass() != obj.getClass()) return false ;
 
         Entry e = (Entry) obj ;
